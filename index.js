709bb3342987316942377dfca79479f2c5c883fc
Mason Ho (TW) <MHo@aromaco.com>
2025-07-17 11:59:53 +0800

    Restore original index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-// 當使用者打開 localhost:3000 時，顯示「Hello World」
+// 當使用者拜訪 localhost:3000 時，顯示 "Hello World"
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.listen(3000, () => {
-  console.log('伺服器運作中 http://localhost:3000')
-})
-
+  console.log('伺服器已啟動 http://localhost:3000');
+});
